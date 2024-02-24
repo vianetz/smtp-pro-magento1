e759722fbcfdfd6816af484358abcf59ca12d6bc
Christoph Massmann <cm@vianetz.com>
2024-02-24 14:07:44 +0100

    fixed issue if Mage_AdminNotification extension is not active

--- app/code/local/Aschroder/SMTPPro/sql/smtppro_setup/mysql4-install-1.1.0.php
+++ app/code/local/Aschroder/SMTPPro/sql/smtppro_setup/mysql4-install-1.1.0.php
@@ -1,7 +1,5 @@
 <?php
 /**
- *
- *
  * @author Ashley Schroder (aschroder.com)
  * @copyright  Copyright (c) 2014 Ashley Schroder
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
